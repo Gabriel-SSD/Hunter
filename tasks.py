7e24a3c588b3c79a54e24f280ff61bb731f889f8
Prison <gabrieltsd@proton.me>
2024-11-15 14:45:15 -0300

    add logging

--- tasks.py
+++ tasks.py
@@ -1,15 +1,13 @@
 import os
 import discord
+from logger import setup_logging
 from dotenv import load_dotenv
 from discord.ext import tasks
 from datetime import datetime
 from bot_utils import plot_ticket_report, get_tickets_missed, format_embed
-import logging
 
 # Set up logging
-logging.basicConfig(filename='bot_tasks.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-
-# Load environment variables
+logger = setup_logging()
 load_dotenv()
 CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
 FILE = "./data/tickets.png"
@@ -21,13 +19,13 @@ async def af_tickets(bot):
     now = datetime.now()
     if now.hour == 17 and now.minute == 31:
         try:
-            logging.info(f"Sending ticket report for 'Awakening Fear' guild at {now}.")
+            logger.info(f"Sending ticket report for 'Awakening Fear' guild at {now}.")
             channel = bot.get_channel(CHANNEL_ID)
             plot_ticket_report("1HE3bh3LRcWVOto5KuGvzQ")
             await channel.send(file=discord.File(fp=FILE))
-            logging.info("Ticket report for 'Awakening Fear' sent successfully.")
+            logger.info("Ticket report for 'Awakening Fear' sent successfully.")
         except Exception as e:
-            logging.error(f"Error sending ticket report for 'Awakening Fear' at {now}: {e}")
+            logger.error(f"Error sending ticket report for 'Awakening Fear' at {now}: {e}")
 
 
 @tasks.loop(minutes=1)
@@ -36,16 +34,16 @@ async def ah_tickets(bot):
     now = datetime.now()
     if now.hour == 22 and now.minute == 31:
         try:
-            logging.info(f"Sending ticket report for 'Awakening Hope' guild at {now}.")
+            logger.info(f"Sending ticket report for 'Awakening Hope' guild at {now}.")
             channel = bot.get_channel(CHANNEL_ID)
             plot_ticket_report("iO-khl_0TVu64OussT1Y7g")
             await channel.send(file=discord.File(fp=FILE))
             df = get_tickets_missed("iO-khl_0TVu64OussT1Y7g", '0')
             embed = format_embed(df, "Awakening Hope", '0')
             await channel.send(embed=embed)
-            logging.info("Ticket report and missed tickets report for 'Awakening Hope' sent successfully.")
+            logger.info("Ticket report and missed tickets report for 'Awakening Hope' sent successfully.")
         except Exception as e:
-            logging.error(f"Error sending ticket report for 'Awakening Hope' at {now}: {e}")
+            logger.error(f"Error sending ticket report for 'Awakening Hope' at {now}: {e}")
 
 
 @tasks.loop(minutes=1)
@@ -54,14 +52,14 @@ async def af_tickets_missed(bot):
     now = datetime.now()
     if now.weekday() == 6 and now.hour == 17 and now.minute == 32:
         try:
-            logging.info(f"Sending missed tickets report for 'Awakening Fear' guild at {now}.")
+            logger.info(f"Sending missed tickets report for 'Awakening Fear' guild at {now}.")
             channel = bot.get_channel(CHANNEL_ID)
             df = get_tickets_missed("1HE3bh3LRcWVOto5KuGvzQ", '7')
             embed = format_embed(df, "Awakening Fear", '7')
             await channel.send(embed=embed)
-            logging.info("Missed tickets report for 'Awakening Fear' sent successfully.")
+            logger.info("Missed tickets report for 'Awakening Fear' sent successfully.")
         except Exception as e:
-            logging.error(f"Error sending missed tickets report for 'Awakening Fear' at {now}: {e}")
+            logger.error(f"Error sending missed tickets report for 'Awakening Fear' at {now}: {e}")
 
 
 @tasks.loop(minutes=1)
@@ -70,11 +68,11 @@ async def ah_tickets_missed(bot):
     now = datetime.now()
     if now.weekday() == 6 and now.hour == 22 and now.minute == 32:
         try:
-            logging.info(f"Sending missed tickets report for 'Awakening Hope' guild at {now}.")
+            logger.info(f"Sending missed tickets report for 'Awakening Hope' guild at {now}.")
             channel = bot.get_channel(CHANNEL_ID)
             df = get_tickets_missed("iO-khl_0TVu64OussT1Y7g", '7')
             embed = format_embed(df, "Awakening Hope", '7')
             await channel.send(embed=embed)
-            logging.info("Missed tickets report for 'Awakening Hope' sent successfully.")
+            logger.info("Missed tickets report for 'Awakening Hope' sent successfully.")
         except Exception as e:
-            logging.error(f"Error sending missed tickets report for 'Awakening Hope' at {now}: {e}")
+            logger.error(f"Error sending missed tickets report for 'Awakening Hope' at {now}: {e}")
